07514b190b73f5f56bfae502e15758917ebfc8cf
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2026-01-22 11:20:31 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../index.css';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,3 +10,4 @@ root.render(
   </React.StrictMode>
 
 );
+
